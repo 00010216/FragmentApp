b5b468db3de84cd5dfd9117b763bed9465c56ddd
00010216 <00010216@uca.edu.sv>
2018-04-16 21:01:43 -0600

    definicion de variables que manejan las vistas

--- app/src/main/java/com/example/rafaj/fragmentapp/Main2Activity.java
+++ app/src/main/java/com/example/rafaj/fragmentapp/Main2Activity.java
@@ -3,17 +3,21 @@ package com.example.rafaj.fragmentapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
-    TextView text;
+    TextView title, description;
+    ImageView img1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        text = findViewById(R.id.textId);
+        title = findViewById(R.id.txt_title);
+        description = findViewById(R.id.txt_description);
+        img1 = findViewById(R.id.img_planet);
 
         Intent callingIntent = getIntent();
         String intentAction = callingIntent.getAction();
@@ -28,10 +32,20 @@ public class Main2Activity extends AppCompatActivity {
     }
 
     private void handleReceivedText(Intent intent){
-        String intentText = intent.getStringExtra(Intent.EXTRA_TEXT);
+        Bundle bundle1 = intent.getExtras();
 
-        if (text != null){
-            text.setText(intentText);
+        if (title != null){
+            title.setText(bundle1.getString("KEY"));
         }
+
+        if (description != null){
+            description.setText(bundle1.getString("KEY1"));
+        }
+
+        if (img1 != null){
+            img1.setImageResource(bundle1.getInt("KEY2"));
+        }
+
+
     }
 }
