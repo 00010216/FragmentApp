15199bebb0fe635a8fefc8c547cb8f5feb6fdfc5
00010216 <00010216@uca.edu.sv>
2018-04-16 20:59:15 -0600

    agregada nueva estructura grafica

---
+++ app/src/main/java/com/example/rafaj/fragmentapp/BasicInformation.java
@@ -0,0 +1,40 @@
+package com.example.rafaj.fragmentapp;
+
+
+
+public class BasicInformation {
+
+    private String  title;
+    private String  image;
+    private String description;
+
+    public BasicInformation (String title, String image, String description){
+        this.title = title;
+        this.image = image;
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
